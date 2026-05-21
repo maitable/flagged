bf21fcfc4dd827f2e7626f6a2586a3fe9ebf31e6
maitable <maiapricop18@gmail.com>
2026-05-21 14:50:41 +0300

    vault done i think

--- src/flagged/vault.py
+++ src/flagged/vault.py
@@ -1,26 +1,32 @@
 import os
 from dataclasses import dataclass
 import tomllib
+from pathlib import Path
 
 @dataclass
 class VaultConfig:
     author: str = ""
     editor: str = ""
-    date_format: str = "%T-%m-%d"
+    date_format: str = "%Y-%m-%d"
 
 def find_vault_root():
-    ls = os.listdir()
-    cwd = os.getcwd()
-    if ".ctf.toml" in ls:
-        return str(cwd) + '/.ctf.toml'
+    start = Path.cwd()
+    for directory in [start, *start.parents]:
+        if(directory / ".ctf.toml").exists():
+            return directory
     return None
     
 def load_config():
-    path = find_vault_root()
-    if path is not None:
-        with open(path, "rb") as toml:
-            toml_dict = tomllib.load(toml)
-    return VaultConfig(author=toml_dict["author"], editor=toml_dict["editor"], date_format=toml_dict["date_format"])
+    root = find_vault_root()
+    if root is None:
+        retirm VaultConfig
+    with open(root / ".ctf.toml", "rb") as toml:
+        toml_dict = tomllib.load(toml)
+    return VaultConfig(
+        author=toml_dict.get("author", ""),
+        editor=toml_dict.get("editor", ""),
+        date_format=toml_dict.get("date_format", "%Y-%m-%d"),
+    )
 
 
 print(load_config())
