2e8739b7e56e23666c3814e93386adb97233fb9c
maitable <maiapricop18@gmail.com>
2026-05-21 15:32:51 +0300

    somehow have stuff working

--- src/flagged/cli.py
+++ src/flagged/cli.py
@@ -1,8 +1,23 @@
 import typer
+from pathlib import Path
 
-app = typer.Typer(help="Flagged")
+app = typer.Typer(help="Flagged", no_args_is_help=True)
 
 @app.command()
 def init():
     """Create a vault in the current directory."""
+    cwd = Path.cwd()
+    vault_file = cwd/".ctf.toml"
+
+    if vault_file.exists():
+        typer.echo("Vault already exists here.")
+        raise typer.Exit(1)
+    vault_file.touch()
+    typer.echo(f"Complete vault init at {cwd}")
+
+    typer.echo("TODO")
+
+@app.command()
+def new():
+    """Meow"""
     typer.echo("TODO")
