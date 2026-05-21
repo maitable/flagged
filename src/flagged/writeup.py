45549e66dabd1f479edd9020b2cbcfc9a23a4306
maitable <maiapricop18@gmail.com>
2026-05-21 15:08:29 +0300

    writeup.py dataclass and load function

---
+++ src/flagged/writeup.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from datetime import date
+from pathlib import path
+import frontmatter
+
+@dataclass
+class Writeup:
+    title : str
+    ctf: str
+    category: str
+    date: date
+    solved: bool=False
+    flag:str=""
+    points:int=0
+    difficulty:str=""
+    tags:list[str]=field(default_factory=list) #default_factory=list creates a new list for each Writeup instance while list[str]=[] wouldve used the same tags for all writeups
+    path: Path = field(default=None, repr=False) #repr=False bc path is not useful output
+    slug:str=""
+    content:str=""
+
+# post["title"] gets a frontmatter field
+# post.content gets the markdown body below ---
+
+
+def load(path:Path) -> Writeup:
+    post=frontmatter.load(path)
+    return Writeup(
+        title = post['title'],
+        ctf = post["ctf"],
+        category = post["category"],
+        date = post["date"],
+        solved = post.get('solved',False),
+        flag = post.get("flag", 0),
+        points = post.get("points", 0),
+        difficulty = post.get("difficulty", ""),
+        tags = post.get("tags", []),
+        path = path,
+        slug=path.stem,
+        content=post.content,
+    )
+
+
